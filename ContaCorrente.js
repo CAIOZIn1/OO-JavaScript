b4294c3016478badadc215ec948635a7e798c45d
Caio Sella <sellacaio4@gmail.com>
2022-11-14 20:13:19 -0300

    conceitos get, set e constructor atribuidos

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,10 +1,31 @@
+import Cliente from './Cliente.js'
+
 export default class ContaCorrente {
     agencia;
-    cliente;
-
-
-    //a convesão para atributos privado é o _, ou seja, no nosso caso deveriamos utilizar _saldo. Não usei porque o meu não deu "erro" :^)
+    _cliente;
     #saldo = 200;
+    //a convesão para atributos privado é o _, ou seja, no nosso caso deveriamos utilizar _saldo. Não usei porque o meu não deu "erro" :^)
+
+    set cliente (novoValor) {
+        if  (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+
+    } //-> usado para "setar", ou seja, inserir uma informação em um atributo privado
+
+    get cliente() {
+        return this._cliente
+    } //-> ele retorna até mesmo atributos privados com a tralha - # -
+    
+
+    get saldo () {
+        return this.#saldo
+    }
+
+    constructor (cliente, agencia) {
+        this.cliente = cliente
+        this.agencia = agencia
+    }
 
     sacar(valor) {
         if (this.#saldo >= valor) {
@@ -16,14 +37,14 @@ export default class ContaCorrente {
     }
 
     depositar(valor) {
-        if (valor < 0) return //-> para evitar muitos if's. Evitar uma identação muito alta
+        if (valor < 0) return; //-> para evitar muitos if's. Evitar uma identação muito alta
         this.#saldo += valor;
         // console.log(this.saldo)
         //só consigo acessa-lo dentro da classe já que o mesmo é um atribubo privado pelo uso da tralha (#) antes da variável
     }
 
     transferir (valor, conta){
-        const valorSacado = this.sacar(valor)
-        conta.depositar(valorSacado)
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import Cliente from "./Cliente.js";
 import ContaCorrente from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = 'Ricardo';
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1
+const cliente2 =  new Cliente("Alice", 88822233309);
 
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.agencia = 102;
-contaCorrenteAlice.cliente = new Cliente()
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+
+const contaCorrenteAlice = new ContaCorrente(cliente2, 102);
+// contaCorrenteAlice.cliente = new Cliente()
+// contaCorrenteAlice.cliente.nome = "Alice"
+// contaCorrenteAlice.cliente.cpf = 88822233309
 //por ser uma referência - o new Cliente - podemos passa-lo diretamnete no atributo cliente da contaCorrenteAlice
-contaCorrenteAlice.cliente.nome = "Alice"
-contaCorrenteAlice.cliente.cpf = 88822233309
+//não é a melhor prática, mesmo porque confunde o código e torna o mesmo muito maior e complexo
 
-contaCorrenteRicardo.transferir(100, contaCorrenteAlice)
+contaCorrenteRicardo.transferir(100, contaCorrenteAlice);
 
-console.log(contaCorrenteRicardo)
-console.log(contaCorrenteAlice)
+console.log(contaCorrenteRicardo); //estrutura toda funcionando
+console.log(contaCorrenteAlice); //estrutura toda funcionando
+console.log(contaCorrenteAlice.cliente) // visualizar como funciona o elemento get com o atributo privado
+console.log(contaCorrenteAlice.saldo) // visualizar como funciona o elemento get com o atributo privado, mesmo com a tralha
+console.log(cliente2.cpf) // visualizar como funciona o elemento get com o atributo privado
